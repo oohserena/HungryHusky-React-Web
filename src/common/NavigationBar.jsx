9e330c2cba30935614c7d15513179ddadee4380d
dumia16 <dujay77@gmail.com>
2023-11-16 18:46:09 -0800

    add more logic to navigation bar

--- src/common/NavigationBar.jsx
+++ src/common/NavigationBar.jsx
@@ -1,30 +1,50 @@
 'use client';
- import * as React from "react";
+import * as React from "react";
 
- export default function NavigationBar(props) {
-  // props represents the component that render the call
-   const handleHomeClick = () => {
-     // handle home button click
-   };
- 
-   const handleLogoutClick = () => {
-     // handle logout button click
-   };
- 
-   return (
-     <nav className="items-center bg-red-700 text-white flex justify-between h-10 px-8 px-5 py-0 max-md:flex max-sm:flex max-sm:flex-col max-sm:self-stretch max-sm:grow-0 max-sm:min-h-[auto] max-sm:items-stretch max-sm:h-auto">
-       <ul className="flex list-none ml-auto max-sm:flex max-sm:flex-col max-sm:grow-0 max-sm:items-stretch max-sm:h-auto max-sm:mx-auto">
-         <li className="text-white font-semibold pr-2.5 max-sm:mx-auto max-sm:pl-2.5">
-           <button onClick={handleHomeClick} aria-label="Home">
-             Home
-           </button>
-         </li>
-         <li className="text-white font-semibold px-2.5 max-sm:mx-auto">
-           <button onClick={handleLogoutClick} aria-label="Logout">
-             Logout
-           </button>
-         </li>
-       </ul>
-     </nav>
-   );
- };
+export default function NavigationBar(props) {
+  const handleButtonClick = (label) => {
+    console.log(`Button clicked: ${label}`);
+    // Add your logic here
+  };
+
+  // const isLoggedIn = props.isLoggedIn;
+    const isLoggedIn = true;
+
+  return (
+    <nav className="items-center bg-red-700 text-white flex justify-between h-10 px-8 px-5 py-0 max-md:flex max-sm:flex max-sm:flex-col max-sm:self-stretch max-sm:grow-0 max-sm:min-h-[auto] max-sm:items-stretch max-sm:h-auto">
+      <ul className="flex list-none ml-auto max-sm:flex max-sm:flex-col max-sm:grow-0 max-sm:items-st max-sm:h-auto max-sm:mx-auto">
+        <li className="text-white font-semibold pr-2.5 max-sm:mx-auto max-sm:pl-2.5">
+          <button onClick={() => handleButtonClick("Home")} aria-label="Home">
+            Home
+          </button>
+        </li>
+        {isLoggedIn ? (
+          <>
+            <li className="text-white font-semibold pr-2.5 max-sm:mx-auto max-sm:pl-2.5">
+              <button
+                onClick={() => handleButtonClick("My Account")}
+                aria-label="My Account"
+              >
+                My Account
+              </button>
+            </li>
+            <li className="-white font-semibold px-2.5 max-sm:mx-auto">
+              <button
+                onClick={() => handleButtonClick("Logout")}
+                aria-label="Logout"
+              >
+                Logout
+              </button>
+            </li>
+          </>
+        ) : (
+          <li className="text-white font-semibold pr-2.5 max-sm:mx-auto max-sm:pl-2.5">
+            <button onClick={() => handleButtonClick("Login")} aria-label="Login">
+              Login
+            </button>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+};
---
+++ src/mia/edit_profile/index.jsx
@@ -0,0 +1,12 @@
+import NavigationBar from "@/common/NavigationBar";
+import Body from "./admin";
+
+export default function AdminView() {
+    return (
+      <div>
+        <NavigationBar />
+        <Body />
+      </div>
+    );
+  }
+  
