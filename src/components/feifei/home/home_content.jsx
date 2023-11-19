9c78394b2cafa11be67b66f986fef4dbefcb2945
Hui Zou <zou.hui@northeastern.edu>
2023-11-19 11:48:07 -0800

    update login view and home page logic

--- src/components/feifei/home/home_content.jsx
+++ src/components/feifei/home/home_content.jsx
@@ -2,51 +2,75 @@ import React, { useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 
 function HomeComponent(props) {
-  const [data, setData] = useState([]);
   const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [recentReviewData, setRecentReviewData] = useState([]);
+  const [recentActivityData, setRecentActivityData] = useState([]);
+  const [categoriesData, setCategoriesData] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    fetchData();
-  }, []);
+    const fetchRecentReviewData = async () => {
+      try {
+        const response = await fetch("API_URL");
+        const data = await response.json();
+        setRecentReviewData(data);
+      } catch (error) {
+        setError(error);
+      }
+    };
 
-  const fetchData = async () => {
-    try {
-      const response = await fetch("API_URL");
-      const data = await response.json();
-      setData(data);
-    } catch (error) {
-      console.error(error);
-    }
-  };
+    const fetchRecentActivityData = async () => {
+      try {
+        const response = await fetch("API_URL");
+        const data = await response.json();
+        setRecentActivityData(data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    const fetchCategoriesData = async () => {
+      try {
+        const response = await fetch("API_URL");
+        const data = await response.json();
+        setCategoriesData(data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchRecentReviewData();
+    fetchRecentActivityData();
+    fetchCategoriesData();
+  }, []);
 
   const handleButtonClick = () => {
     // handle button click event here
+    // Use react-router-dom to navigate to the search page
+    // Example: history.push("/search");
   };
 
-  const renderColumns = () => {
-    return data.map((item, i) => (
-      <div
-        key={i}
-        className="flex flex-col items-stretch w-6/12 max-md:w-full max-md:ml-0"
-      >
+  const renderRecentReviewColumns = () => {
+    return recentReviewData.map((review) => (
+      <div key={review.id} className="flex flex-col items-stretch w-6/12 max-md:w-full max-md:ml-0">
         <div className="flex flex-col relative shrink-0 box-border h-auto border m-5 pb-8 border-solid border-neutral-400">
           <div className="flex flex-col relative shrink-0 box-border my-2.5">
             <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
               <div className="flex flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
                 <div className="relative shrink-0 box-border h-auto ml-1.5 my-2.5">
                   <font color="#4a4a4a">
-                    <b>{item.restaurantName}</b>
+                    <b>{review.restaurantName}</b>
                   </font>
                 </div>
                 <img
                   loading="lazy"
-                  srcSet={item.imageSrc}
+                  srcSet={review.imageSrc}
                   className="aspect-[1.25] object-cover object-center w-full shrink-0 box-border min-h-[20px] min-w-[20px] overflow-hidden ml-1.5 mb-2.5"
                 />
               </div>
               <div className="flex flex-col items-stretch w-[67%] ml-5 max-md:w-full max-md:ml-0">
                 <div className="relative shrink-0 box-border h-auto mt-2.5">
-                  <font color="#4a4a4a">{item.review}</font>
+                  <font color="#4a4a4a">{review.reviewText}</font>
                 </div>
               </div>
             </div>
@@ -56,13 +80,44 @@ function HomeComponent(props) {
     ));
   };
 
+  const renderRecentActivityItems = () => {
+    return recentActivityData.map((activity) => (
+      <div key={activity.id} className="flex flex-col items-stretch w-[33%] max-md:w-full max-md:ml-0">
+        <div className="flex flex-col relative shrink-0 box-border h-auto border m-5 border-solid border-neutral-400">
+          <div className="relative shrink-0 box-border h-auto my-2.5">{activity.foodieName}</div>
+          <img
+            loading="lazy"
+            srcSet={activity.imageSrc}
+            className="aspect-[2] object-cover object-center w-full shrink-0 box-border min-h-[20px] min-w-[20px] overflow-hidden my-2.5"
+          />
+          <div className="relative shrink-0 box-border h-auto my-5">{activity.reviewInfo}</div>
+        </div>
+      </div>
+    ));
+  };
+
+  const renderCategories = () => {
+    return categoriesData.map((category) => (
+      <div key={category.id} className="flex flex-col items-stretch w-3/12 max-md:w-full max-md:ml-0">
+        <div className="flex flex-col relative shrink-0 box-border h-auto border ml-8 mr-5 my-5 border-solid border-neutral-400">
+          <img
+            loading="lazy"
+            srcSet={category.imageSrc}
+            className="aspect-[1.1] object-cover object-top w-full shrink-0 box-border min-h-[20px] min-w-[20px] overflow-hidden mb-5"
+          />
+          <div className="relative shrink-0 box-border h-auto self-center mt-5">{category.name}</div>
+        </div>
+      </div>
+    ));
+  };
+
   return (
     <main className="flex flex-col relative shrink-0 box-border bg-white min-h-[auto]">
-      <section className="relative shrink-0 box-border flex flex-col items-stretch min-h-[472px] bg-[url(https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fe85b95ec3135499f97c84d225197ea35)] bg-cover bg-no-repeat bg-center mb-5">
+      <section className="relative shrink-0 box-border flex flex-col items-stretch min-h-[472px] bg-[url(https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fe85b95ec3135499f97c84d225197ea35)] bg-no-repeat bg-center bg-cover mb-5 max-md:min-h-[auto] max-sm:font-thin max-sm:min-h-[auto]">
         <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
           <div className="flex flex-col items-stretch w-full max-md:w-full max-md:ml-0">
             <div className="flex flex-col relative shrink-0 box-border h-auto max-w-[695px] min-h-[170px] mt-36 mb-24 mx-auto pb-1.5">
-              <div className="flex flex relative shrink-0 box-border min-[auto] mx-auto my-2.5 max-md:w-auto max-md:h-auto max-md:min-h-[auto] max-sm:w-auto max-sm:h-auto max-sm:min-h-[auto] max-sm:mx-auto">
+              <div className="flex flex-col relative shrink-0 box-border min-h-[auto] mx-auto my-2.5 max-md:w-auto max-md:h-auto max-md:min-h-[auto] max-sm:w-auto max-sm:h-auto max-sm:min-h-[auto] max-sm:mx-auto">
                 <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
                   <div className="flex flex-col items-stretch w-full max-md:w-full max-md:ml-0">
                   <form className="w-full max-w-xs mx-auto space-y-5"> 
@@ -98,6 +153,7 @@ function HomeComponent(props) {
           </div>
         </div>
       </section>
+
       {isLoggedIn && (
         <section className="flex flex-col relative shrink-0 box-border my-5">
           <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
@@ -107,29 +163,31 @@ function HomeComponent(props) {
               </div>
               <div className="relative shrink-0 box-border flex-col flex mt-5 mx-6">
                 <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-                  {renderColumns()}
+                  {renderRecentReviewColumns()}
                 </div>
               </div>
             </div>
           </div>
         </section>
       )}
+
       <section className="relative shrink-0 box-border h-auto text-center text-3xl font-semibold tracking-wider mx-auto my-5 p-5">
         Recent Activity
       </section>
-      {isLoggedIn && (
-        <section className=" flex-col relative shrink-0 box-border mt-5 mx-16">
-          <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-            {/* Render recent activity items */}
-          </div>
-        </section>
-      )}
-      <section className="relative shrink-0 box-border h-auto text-center text-3xl font-semibold tracking-wider mx-auto my-5 p-5">
-        <p>Categories</p>
+
+      <section className="flex flex-col relative shrink-0 box-border mt-5 mx-16">
+        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
+          {renderRecentActivityItems()}
+        </div>
       </section>
+
+      <section className="relative shrink-0 box-border h-auto text-center text-3xl font-semibold tracking-wider mx-auto my-5 p-5">
+        Categories
+      </section>
+
       <section className="flex flex-col relative shrink-0 box-border mx-16 my-5">
         <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-          {/* Render categories */}
+          {renderCategories()}
         </div>
       </section>
     </main>
--- src/components/feifei/log_in/login_form.jsx
+++ src/components/feifei/log_in/login_form.jsx
@@ -30,66 +30,70 @@ export default function LoginForm(props) {
   };
 
   return (
-    
-    <section className="flex flex-col relative shrink-0 box-border bg-white min-h-[200px] pb-24 px-24 max-md:flex max-sm:flex">
-      <div className="flex flex-col relative shrink-0 box-border mt-5 mb-1.5">
-        <div className="gap-5 flex max-md:flex-col max-md:items-stretch max-md:gap-0">
-          <div className="flex flex-col items-stretch-6/12 max-md:w-full max-md:ml-0">
-            <header className="relative shrink-0 box-border h-auto text-red-700 mt-52 mx-auto max-md:mt-5 max-sm:w-auto max-sm:self-center max-sm:m-auto">
-              <h1>
-                <span className="mx-auto">Log in to XX</span>
-              </h1>
-            </header>
-            <form onSubmit={handleFormSubmit}>
-              <label>
-                <input
-                  type="email"
-                  placeholder="Email"
-                  name="email"
-                  className="flex flex-col relative shrink-0 box-border border mt-5 p-2.5 rounded border-solid border-stone-300"
-                  required={true}
-                />
-              </label>
-              <label>
-                <input
-                  type="password"
-                  placeholder="Password"
-                  name="Password"
-                  className="flex flex-col relative shrink-0 box-border border mt-5 p-2.5 rounded border-solid border-stone-300"
-                  required={true}
-                />
-              </label>
-              <a
-                href=""
-                className="relative shrink-0 box-border h-auto text-blue-500 ml-auto mt-5 max-sm:ml-auto"
-                onClick={handleForgotPasswordClick}
-              >
-                Forgot password?
-              </a>
-              <button
-                type="submit"
-                className="relative shrink-0 box-border appearance-none bg-red-700 text-[white] rounded text-center cursor-pointer h-[50px] font-semibold mt-5 px-6 py-4"
-                openLinkInNewTab={true}
-              >
-                Log In
-              </button>
-              <div className="relative shrink-0 box-border h-auto ml-auto mt-5 mb-auto max-sm:ml-auto">
-                Don't have an account?&nbsp;{" "}
-                <font color="#4a90e2" onClick={handleSignUpClick}>
-                  Sign up
-                </font>
-              </div>
-            </form>
-          </div>{" "}
-          <div className="flex flex-col items-stretch w-6/12 ml-5 max-md:w-full max-md:ml-0">
-            <img
-              loading="lazy"
-              srcSet="https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=100 100w, https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=200 200w, https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=400 400w, https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=800 800w, https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=1200 1200w, https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=1600 1600w, https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=2000 2000w, https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0"className="aspect-[0.68] object-contain object-center w-full shrink-0 box-border min-w-[20px] overflow-hidden ml-auto mt-5"
+    <div className="flex min-h-screen">
+      {" "}
+      {/* Full screen height and display flex */}
+      <section className="w-1/2 flex flex-col justify-start items-center bg-white pt-100 p-24">
+        {" "}
+        {/* Align to the top and add padding to the top */}
+        <div className="w-full max-w-md">
+          {" "}
+          {/* Limit the width of the form */}
+          <h1 className="text-red-700 text-center text-4xl mb-6">
+            Log in to XX
+          </h1>
+          <form onSubmit={handleFormSubmit} className="flex flex-col gap-5">
+            <input
+              type="email"
+              placeholder="Email"
+              name="email"
+              className="p-2.5 rounded border border-solid border-stone-300 w-full"
+              required={true}
             />
-          </div>
+            <input
+              type="password"
+              placeholder="Password"
+              name="Password"
+              className="p-2.5 rounded border border-solid border-stone-300 w-full"
+              required={true}
+            />
+            <a
+              href="#"
+              className="text-blue-500 text-right"
+              onClick={handleForgotPasswordClick}
+            >
+              Forgot password?
+            </a>
+            <button
+              type="submit"
+              className="bg-red-700 text-white rounded h-12 font-semibold"
+            >
+              Log In
+            </button>
+            <p className="text-center mt-4">
+              Don't have an account?&nbsp;
+              <a href="#" className="text-blue-500" onClick={handleSignUpClick}>
+                Sign up
+              </a>
+            </p>
+          </form>
         </div>
+      </section>
+      <div className="w-1/2  p-20 bg-white">
+        <img
+          loading="lazy"
+          srcSet="https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=100 100w, 
+          https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=200 200w, 
+          https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=400 400w,
+          https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=800 800w, 
+          https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=1200 1200w, 
+          https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=1600 1600w, 
+          https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0?width=2000 2000w,
+          https://cdn.builder.io/api/v1/image/assets%2F92012c9ce23446e48cb46842c120eec0%2Fd5ed74b25d1c4840a59d56b3ddc9eed0"
+          alt="Login Visual"
+          className="object-cover w-full h-full"
+        />
       </div>
-    </section>
-  
+    </div>
   );
-};
+}
