e513ba1fcf35802e8a498339d8a5ee29e4f884a1
dumia16 <dujay77@gmail.com>
2023-12-08 21:54:34 -0800

    delete search_page

--- src/app/search_page/page.jsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-import SearchPage from "@/components/serena/search_page";
-
-import { BrowserRouter } from "react-router-dom";
-
-export default function Page() {
-  return <BrowserRouter><SearchPage /></BrowserRouter>;
-}
